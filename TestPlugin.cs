177b73158061d8da0828f83a8481607862de7042
Wouter van den Heuvel <exo_00@hotmail.com>
2020-05-10 10:46:39 +0200

    initial commit

---
+++ TestPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DarkRift.Server;
+
+public class TestPlugin : Plugin
+{
+    public TestPlugin(PluginLoadData pluginLoadData) : base(pluginLoadData)
+    {
+        WriteEvent("constructor", DarkRift.LogType.Info);
+    }
+
+    public override bool ThreadSafe => false;
+
+    public override Version Version => new Version(1, 0, 0);
+}
